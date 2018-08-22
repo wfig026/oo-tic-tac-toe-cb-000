b303454a381c180859e02c7900feb76d9af29d4c
Will Figueiredo <will@wfigueiredo.com>
2018-08-22 01:30:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,7 +13,7 @@ class TicTacToe
     [2,4,6]  # right to left diagonal
   ]
   def display_board
-    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
